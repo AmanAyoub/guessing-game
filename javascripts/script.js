d206c7a376170fc87c08a92a136ec98d96d14047
AmanAyoub <aman.ayoub16@gmail.com>
2025-12-10 11:45:52 +0430

    validate the guess

--- javascripts/script.js
+++ javascripts/script.js
@@ -4,4 +4,25 @@ document.addEventListener("DOMContentLoaded", e => {
   }
 
   let answer = numberBetween(1, 100);
+  let submit = document.querySelector("input[type='submit'");
+  let input = document.querySelector("#guess");
+  let guessesCount = 0;
+
+  submit.addEventListener("click", event => {
+    event.preventDefault();
+    
+    let guess = parseInt(input.value, 10);
+    let message;
+    guessesCount += 1;
+
+    if (guess === answer) {
+      message = `You guessed it! It took you ${guessesCount} guesses`;
+    } else if (guess > answer) {
+      message = `My number is lower than ${guess}`;
+    } else if (guess < answer) {
+      message = `My number is higher than ${guess}`;
+    }
+
+    console.log(message);
+  });
 });
