c76bd494fb4e2a221ad4e90b2733c842c7597a5d
AmanAyoub <aman.ayoub16@gmail.com>
2025-12-10 11:00:17 +0430

    random number

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", e => {
+  function numberBetween(min, max) {
+    return Math.floor(Math.random() * max) + min;
+  }
 
+  let answer = numberBetween(1, 100);
 });
